72835a72d7ea380de572fb6e89178828b4d52222
I <henrikl@tuta.io>
2022-10-26 08:29:33 +0200

    unspecified

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const LAMP_SERVER_PREFIX = "http://192.168.0.198:8000/deviceControl.html?jsonUrl=itemContainerJson"
+const SET_WIRELESS_CHARGER_STATE = `${LAMP_SERVER_PREFIX}&device={"id":65539,"name":"wireless charger"}&action=setState`
+
 function main(){
     const queryString = window.location.search
     const urlParams = new URLSearchParams(queryString);
@@ -13,10 +16,11 @@ function main(){
 function destinationRouter(destination){
     switch(destination){
         case "eveninglight":
-            return 'http://192.168.0.198:8000/deviceControl.html?jsonUrl=itemContainerJson'
-                + '&device={"id":65541,"name":"bedroom bulb"}'
-                +'&action=setColor'
-                + '&payload=da5d41'
+            return `${LAMP_SERVER_PREFIX}&device={"id":65541,"name":"bedroom bulb"}&action=setColor&payload=da5d41`
+        case "turnOnWirelessCharger":
+            return `${SET_WIRELESS_CHARGER_STATE}&payload=1`
+        case "turnOffWirelessCharger":
+            return `${SET_WIRELESS_CHARGER_STATE}&payload=0`
         default: return destination;
     }
 }
